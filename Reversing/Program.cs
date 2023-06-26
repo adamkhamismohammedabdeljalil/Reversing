1c9260cab52fef7337dc436c5c33b136c89aabcb
adamkhamismohammedabdeljali <adamkhamismohammedabdeljalil@gmail.com>
2023-06-26 16:43:31 +0200

    feat:add Reverse project

---
+++ Reversing/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Reversing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please insert a text to be reversed");
+            Reverse reversed = new Reverse();
+            string text=Console.ReadLine(); 
+            reversed.ReverseMethod(text);
+        }
+    }
+}
---
+++ Reversing/Reverse.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reversing
+{
+    internal class Reverse
+    {
+        //public string Input
+        //{
+        //    get; set;   
+        //}
+        public void ReverseMethod(string input)
+        {
+            
+            string reverse = string.Empty;
+            char[] array= input.ToCharArray();  
+            for(int i=array.Length-1; i>=0; i--)
+            {
+                reverse += array[i];
+            }
+            Console.WriteLine(reverse);
+        }
+    }
+}
